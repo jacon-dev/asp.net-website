99aa49cdfb36b2148e85aaaf093e5622eba9f66d
James Conlin <54316600+jacon-dev@users.noreply.github.com>
2020-06-18 14:36:43 +0100

    Formatting change for Index
    Fixed the formatting for the Latest Blog section which now includes a datetime for published and dynamic link generation

--- PersonalSite/PersonalSite.DataAccess/InMemoryDataAccess.cs
+++ PersonalSite/PersonalSite.DataAccess/InMemoryDataAccess.cs
@@ -12,11 +12,11 @@ namespace PersonalSite.DataAccess
         {
             return new List<BlogSummary>
             {
-                new BlogSummary { BlogTitle = "Welcome to the Blog!", BlogPageLink = "//Blog//WelcomeToTheBlog", PublishDate = new DateTime(2020, 6, 10) },
-                new BlogSummary { BlogTitle = "In Memory Data", BlogPageLink = "//Blog//InMemoryData", PublishDate = new DateTime(2020, 6, 11) },
-                new BlogSummary { BlogTitle = "Patience is Key", BlogPageLink = "//Blog//PatienceIsKey", PublishDate = new DateTime(2020, 6, 13) },
-                new BlogSummary { BlogTitle = "FizzBuzz - The Interview", BlogPageLink = "//Blog//FizzBuzz", PublishDate = new DateTime(2020, 6, 15) },
-                new BlogSummary { BlogTitle = "Leadsership vs Management", BlogPageLink = @"/Blog/LeaderVsManager", PublishDate = new DateTime(2020, 6, 18) }
+                new BlogSummary { BlogTitle = "Welcome to the Blog!", BlogPageLink = "/Blog/WelcomeToTheBlog", PublishDate = new DateTime(2020, 6, 10) },
+                new BlogSummary { BlogTitle = "In Memory Data", BlogPageLink = "/Blog/InMemoryData", PublishDate = new DateTime(2020, 6, 11) },
+                new BlogSummary { BlogTitle = "Patience is Key", BlogPageLink = "/Blog/PatienceIsKey", PublishDate = new DateTime(2020, 6, 13) },
+                new BlogSummary { BlogTitle = "FizzBuzz - The Interview", BlogPageLink = "/Blog/FizzBuzz", PublishDate = new DateTime(2020, 6, 15) },
+                new BlogSummary { BlogTitle = "Leadsership vs Management", BlogPageLink = "/Blog/LeaderVsManager", PublishDate = new DateTime(2020, 6, 18) }
             }.OrderByDescending(r => r.PublishDate);
         }
     }
